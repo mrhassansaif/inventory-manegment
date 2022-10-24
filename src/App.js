6924aac834ab0e47c454139d5dbd93034df35050
Muhammad Hassan Bin Saif <hassansaif0ki@gmail.com>
2022-10-24 14:45:38 +0500

    Added Auth Component
    Added Auth Component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import Auth from './Components/Auth/Auth';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mene changes ki hain
-        </p>
-        <p>
-          <b>Mene changes ki hain 2.0</b>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Auth />
   );
 }
 
---
+++ src/Components/Auth/Auth.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import SignInSide from "./SignupSignin/SignIn";
+import SignUpSide from './SignupSignin/SignUp';
+
+
+export default function Auth() {
+  return (
+    <SignInSide />
+    // <SignUpSide />
+  )
+}
+
